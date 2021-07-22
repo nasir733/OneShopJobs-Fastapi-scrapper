00c8629f5c86bdb622ded4b42e91546780ec20f7
nasir <engnasir733@gmail.com>
2021-07-22 22:24:52 +0500

    make it better

---
+++ main.py
@@ -0,0 +1,67 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.contrib.fastapi import register_tortoise
+from tortoise.contrib.pydantic import pydantic_model_creator
+from stackoverflowscrapper import extract_jobs
+from fastapi.responses import JSONResponse
+import time
+import os
+import asyncio
+from models import JobCategory, Jobs_Pydantic, Jobs
+import uvicorn
+# from dotenv import dotenv_values
+from fastapi.middleware.cors import CORSMiddleware
+# config = dotenv_values(".env")
+config ={}
+app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:9000",
+    'http://'
+]
+
+
+@app.get("/get-jobs")
+async def getJobs():
+    return await Jobs_Pydantic.from_queryset(Jobs.all())
+
+
+@app.get("/create-category")
+async def createCategory():
+    return await JobCategory.create(name="Python")
+
+
+@app.get("/scrape-stackoverflow")
+async def startAsyncScrapper():
+    print("hello from stackoverflow jobs section")
+
+    actions = []
+    start_time = time.time()
+    jobsName = await JobCategory.all()
+    print(jobsName)
+    jobsNames = ['python', 'javascript', 'django', 'channels']
+    # print(jobsName)
+    for job in jobsName:
+        url = f"https://stackoverflow.com/jobs?q={job}"
+        actions.append(asyncio.ensure_future(extract_jobs(9, url, job)))
+    await asyncio.gather(*actions)
+    total_time = time.time() - start_time
+
+    return JSONResponse(f'the async stackoverflow scrapper is done in {total_time} seconds   ')
+
+
+register_tortoise(
+    app,
+    # db_url='postgres://postgres:24242424@database-1.cgflmpcc3zf6.us-east-2.rds.amazonaws.com:5432/webscrapper',
+    db_url=f'postgres://{config["USER"] or os.environ.get("USER")}:{config["PASSWORD"] or os.environ.get("PASSWORD")}@{config["HOST"] or os.environ.get("HOST")}:5432/{config["NAME"] or os.environ.get("NAME")}',
+    modules={'models': ['main']},
+    generate_schemas=True,
+    add_exception_handlers=True
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app,  port=4000)
---
+++ stackoverflowscrapper.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+import requests
+from icecream import ic
+import aiohttp
+import time
+from models import Jobs, JobCategory
+
+
+async def get_last_page(url):
+    result = requests.get(url)
+    soup = BeautifulSoup(result.text, "html.parser")
+    pages = soup.find("div", {"class": "s-pagination"}).find_all("a")
+    last_page = pages[-2].get_text(strip=True)
+    return int(last_page)
+
+
+async def extract_job(html, name, url):
+    soup = html.find("div", {"class": "fl1"})
+    title = soup.find("h2").find("a")["title"]
+    link = soup.find("h2").find("a")["href"]
+    companies, locations = soup.find("h3").find_all("span", recursive=False)
+    # recursive all for only 2 spans to avoid going deeper spans
+    company = companies.get_text(strip=True).strip(" \r").strip("\n")
+    location = locations.get_text(strip=True).strip(" \r").strip("\n")
+
+    link_url=f"https://stackoverflow.com/{link}"
+    """ try and except are only for error handling because in async
+     the tortoise orm get_or_create method can return 2 or more object and 
+     causing the program to fail"""
+
+    try:
+        await Jobs.get_or_create(
+            title=title,
+            company_name=company,
+            location=location,
+            jobCategory=name,
+            link=link_url,
+            job_by="stackoverflow",
+        )
+    except Exception as e:
+        print(e)
+        pass
+
+
+async def extract_jobs(last_page, url, name):
+    print(f"geting data for {name}")
+    async with aiohttp.ClientSession() as session:
+        for page in range(1, last_page):
+            async with session.get(f"{url}&pg={page}") as resp:
+                result = await resp.text()
+                soup = BeautifulSoup(result, "html.parser")
+                results = soup.find_all("div", {"class": "-job"})
+                for resul in results:
+                    job = await extract_job(resul, name, url)
